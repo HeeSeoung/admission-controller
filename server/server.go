7e63189ad4ca9d71564b809f0e10d4a4b07f4211
희승 <heeseung@huiseung-ui-MacBookPro.local>
2024-02-07 23:08:30 +0900

    feat: update daemonsets

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/HeeSeoung/admission-controller/daemonsets"
 	"github.com/HeeSeoung/admission-controller/deployments"
 	"github.com/HeeSeoung/admission-controller/nodes"
 )
@@ -13,13 +14,16 @@ func NewServer(port string) *http.Server {
 	// Instances hooks
 	deploymentMutation := deployments.NewMutationHook()
 	nodeMutation := nodes.NewMutationHook()
+	daemonsetMutation := daemonsets.NewMutationHook()
 
 	// Routers
 	ah := newAdmissionHandler()
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", healthz())
 	mux.Handle("/mutate/deployments", ah.Serve(deploymentMutation))
+	mux.Handle("/mutate/daemonsets", ah.Serve(daemonsetMutation))
 	mux.Handle("/mutate/nodes", ah.Serve(nodeMutation))
+	
 
 	return &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
